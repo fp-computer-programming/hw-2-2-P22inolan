3e592c896b9bd08d490d262c3309cc620d4de363
P22inolan <p22inolan@fairfieldprep.org>
2021-09-20 10:17:13 -0400

    HW submit 9/20

---
+++ hw2-2.py
@@ -0,0 +1,8 @@
+# Author: IBN (AMDG) 9/20/2021
+free_throws = 3
+two_points = 10
+three_points = 7
+
+score = ((free_throws) + (two_points * 2) + (three_points * 3))
+
+print("The player scored " + str(score) + " points in the game")
